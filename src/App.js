815db44114652df62232d26bf809c57f5e4cbe67
Elaine <sosohn@gmail.com>
2023-02-13 14:35:09 -0800

    Add MainNav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import BottomNav from './components/BottomNav';
 
 function App() {
+
   return (
     <div className="App">
-      <p>hello</p>
+      <BottomNav />
     </div>
   );
 }
