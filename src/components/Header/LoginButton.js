50ed3460570aae93a2d02ebec70b247fbb606e5f
annadu <duyuan0921@gmail.com>
2023-02-13 19:31:32 -0800

    add login button

---
+++ src/components/Header/LoginButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import './LoginButton.css'
+
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <>
+      <button className="login_button"   onClick={() => loginWithRedirect()}>Log In</button>
+    </>
+    );
+
+};
+
+export default LoginButton;
