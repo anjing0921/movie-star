3af4f72989b9281a769da6e90c8a0b1dc9255061
annadu <duyuan0921@gmail.com>
2023-02-13 22:47:28 -0800

    add trending.js

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import './Trending.css'
 
 const Trending = () => {
-  return (
-    <span className='pageTitle'>Trending</span>
-  )
-}
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  
+  
 
-export default Trending
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    //console.log(data.results);
+    setContent(data.results);
+  };
+  useEffect(() => {
+    fetchTrending();
+  }, [page]);
+
+  return (
+    <div>
+      <span className="pageTitle">Trending</span>
+      <div className="trending">
+        {content.map((c)=> (
+          <SingleContent 
+          key={c.id}
+          id={c.id}
+          poster={c.poster_path}
+          title={c.title || c.name}
+          date={c.first_air_date || c.release_date}
+          media_type={c.media_type}
+          vote_average={c.vote_average}/>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
