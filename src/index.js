4004b2b62ab0f0020b92c6d72000ab5d322bfd18
annadu <duyuan0921@gmail.com>
2023-02-13 19:57:08 -0800

    add auth0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-sixj4sjj6nsnsqf0.us.auth0.com"
+      clientId="vkOMbXsvaw0yJDSJ0ENuT2mvdIZABQZf"
+      authorizationParams={{
+        redirect_uri: "https://www.dealmoon.com"
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
