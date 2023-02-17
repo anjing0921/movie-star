ed596bddc744adbc66d44d1de458080ef1b02dd2
annadu <duyuan0921@gmail.com>
2023-02-16 20:19:53 -0800

    SingleContent

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,31 +1,33 @@
 import React from 'react'
 import './SingleContent.css'
 import Badge from '@mui/material/Badge';
+import ContentModal from '../ContentModal/ContentModal';
 
 const SingleContent = ({id,
   poster,
   title,
   date,
   media_type,
-  vote_average,}) => {
+  vote_average,
+  setWatchList,watchlist}) => {
   return (
     <>
     
-    <div className='media'>
-    <Badge  badgeContent={vote_average.toFixed(1)}
+    <ContentModal media_type={media_type} id={id} setWatchList={setWatchList} watchlist={watchlist}>
+      <Badge  badgeContent={vote_average.toFixed(1)}
             color={vote_average > 6 ? "primary" : "secondary"}
       />
-    <img
+      <img
         className="poster"
         src={`https://image.tmdb.org/t/p/w200${poster}`}
         alt={`${title} Poster`}
       />
-    <b className="title">{title}</b>
-    <span className="subTitle">
+      <b className="title">{title}</b>
+      <span className="subTitle">
       {media_type === "tv" ? "TV Series" : "Movie"}
-    <span className="subTitle">{date}</span>
-    </span>
-    </div>
+        <span className="subTitle">{date}</span>
+      </span>
+    </ContentModal>
     </>
   )
 }
