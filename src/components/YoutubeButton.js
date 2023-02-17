1017421cb5ceeb1598429318cd187c23fda8c7dc
annadu <duyuan0921@gmail.com>
2023-02-16 20:14:08 -0800

    add Youtube button

---
+++ src/components/YoutubeButton.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import { createTheme } from '@mui/material/styles';
+import red from '@mui/material/colors/red';
+
+const theme = createTheme({
+  palette: {
+    primary: red,
+  },
+});
+
+
+export default function YoutubeButton({video}) {
+  //console.log(1)
+  return (
+      <Button variant="contained" 
+      color="error"
+      href={`https://www.youtube.com/watch?v=${video}`}
+      startIcon={<YouTubeIcon />}>
+        Watch the Trailer
+      </Button>
+  );
+}
