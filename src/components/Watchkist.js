6d416e9c989b2a5b2c6733782a73b5dbac0ee9d3
annadu <duyuan0921@gmail.com>
2023-02-16 20:30:23 -0800

    add watchlist

---
+++ src/components/Watchkist.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+const Watchlist = ({setWatchList, watchlist}) => {
+  
+  return (
+    <div className="movie-page">
+      <div className="container">
+        <div >
+          <span className="pageTitle">Watch list</span>
+        </div>
+
+        <div className="movie-grid">
+            {watchlist.map((movie) => (
+              <MovieCard movie={movie} key={movie.id}  />
+            ))}
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+   
+ 
+
+export default Watchlist
