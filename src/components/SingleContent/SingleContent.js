69dd0799b67479e7ee8ac940f4e5177105a09a40
Elaine <sosohn@gmail.com>
2023-02-18 16:47:27 -0800

    add checkbox to add my watchlist

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './SingleContent.css'
+import { Box } from '@mui/system';
 import Badge from '@mui/material/Badge';
 import ContentModal from '../ContentModal/ContentModal';
 import Rating from '@mui/material/Rating'
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 const SingleContent = ({id,
   poster,
@@ -11,9 +14,17 @@ const SingleContent = ({id,
   media_type,
   vote_average
 }) => {
-  
+
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <>
+    <Box>
+    <Box>
     <ContentModal media_type={media_type} id={id} >
       <Badge  badgeContent={vote_average.toFixed(1)}
             color={vote_average > 6 ? "primary" : "secondary"}
@@ -30,6 +41,15 @@ const SingleContent = ({id,
         <span className="subTitle">{date}</span>
       </span>
     </ContentModal>
+    </Box>
+    <Box>
+      <FormControlLabel
+        control={<Checkbox onChange={handleChange} />}
+        label="Add to My Watchlist" sx={{color:'black'}}
+      />
+    </Box>
+    </Box>
+    
     </>
   )
 }
