0c7d6d5c83d0ea15ea7d47e35c2ae6c3d746895c
Elaine <sosohn@gmail.com>
2023-02-18 19:39:57 -0800

    Add delete movie from Watchlist

--- src/components/MovieControl.js
+++ src/components/MovieControl.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, {useContext} from "react";
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
+import AuthContext from "../store/auth-context";
+import FetchContext from "../store/fetch-context";
 
+const MovieControl = ({movie}) => {
 
-const MovieControl = () => {
+  const authCtx = useContext(AuthContext);
+  const fetchCtx = useContext(FetchContext);
+  const watchlist_id = movie.watchlists[0].id
+  
 
-  // const {
-  //   removeMovieFromWatchlist
-  // } = useContext(GlobalContext);
-
-
+  const deleteHandler = () => {
+    fetchCtx.onDelete(watchlist_id);
+    fetchCtx.getWatchlist(authCtx.token)
+    }
 
   return (
     <>
     <Button variant="outlined" startIcon={<DeleteIcon />}
-            //onClick={() => removeMovieFromWatchlist(movie.id)}
-    >
-        Delete
-      </Button>
+            onClick={deleteHandler}
+    > Delete
+    </Button>
 
     </>
   )
